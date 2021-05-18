37f3e8731b2bb4099f7657ead7a18af2ab69abf4
Dmitry <dmitryR1997@gmail.com>
2021-05-18 15:17:33 +0300

    fix redirect

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -6,22 +6,17 @@ export default class MyDocument extends Document {
     if(ctx.req && ctx.req.headers && ctx.req.headers["user-agent"]) {
       const detect = getMobileDetect(ctx.req.headers["user-agent"])
 
-
       if (detect.isDesktop()) {
         let desktopUrl = false
         const path = ctx.asPath.split("/")
 
-        if (path) {
-          if (path[1] === "catalog" || path[1] === "vendors" || path[1] === "stati" || path[1] === "content") {
-            desktopUrl = `https://massagery24.ru${ctx.asPath}`
-          } else if (path[1] === "contacts") {
-            desktopUrl = `https://massagery24.ru/content/contacts/`
-          } else if (path[1] === "promotions") {
-            desktopUrl = `https://massagery24.ru/actions/set/`
-          } else if (path[1] === "") {
-            desktopUrl = `https://massagery24.ru/`
-          }
-        } else {
+        if (path[1] === "catalog" || path[1] === "vendors" || path[1] === "stati" || path[1] === "content") {
+          desktopUrl = `https://massagery24.ru${ctx.asPath}`
+        } else if (path[1] === "contacts") {
+          desktopUrl = `https://massagery24.ru/content/contacts/`
+        } else if (path[1] === "promotions") {
+          desktopUrl = `https://massagery24.ru/actions/set/`
+        } else if (path[1] === "") {
           desktopUrl = `https://massagery24.ru/`
         }
 
